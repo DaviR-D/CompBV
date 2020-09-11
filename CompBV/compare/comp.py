ed2a15c9d2f3c56a42395898ffeb48bf2a311cc0
Ortomis <DaviR-D>
2020-09-11 11:23:45 -0300

    Otimização

--- CompBV/compare/comp.py
+++ CompBV/compare/comp.py
@@ -16,7 +16,11 @@ def folderManager(folder1, folder2, clean=0):
         folderListCache = []
 
 
-    fileList1, fileList2, path1, path2 = loadFiles(folder1, folder2)
+    fileList1, path1, dirList1 = loadFiles(folder1)
+    fileList2, path2, dirList2 = loadFiles(folder2)
+
+    compFolders(dirList1, dirList2)
+
     comp1 = Thread(target = compFiles, args = (fileList1, fileList2, path1, folder2))
     comp2 = Thread(target = compFiles, args = (fileList2, fileList1, path2, folder1))
 
@@ -45,35 +49,26 @@ def compFiles(fileList1, fileList2, path1, path2):
         x = True
 
 # Armazena o tamanho dos arquivos em bytes em listas para que as pastas possam ser comparadas simultâneamente
-def loadFiles(folder1, folder2):
-    sizesList1 = []
-    sizesList2 = []
-    path1 = []
-    path2 = []
-    dirList1 = []
-    dirList2 = []
+def loadFiles(folder):
+    sizesList = []
+    path = []
+    dirList = []
 
-    for f in glob.glob(folder1 + '/*'):
+
+    for f in glob.glob(folder + '/*'):
         if not os.path.isdir(f):
-            sizesList1 = sizesList1 + [os.path.getsize(f)]
-            path1 = path1 + [f]
+            sizesList = sizesList + [os.path.getsize(f)]
+            path = path + [f]
         else:
-            dirList1 = dirList1 + [f]
+            dirList = dirList + [f]
 
-    for f in glob.glob(folder2 + '/*'):
-        if not os.path.isdir(f):
-            sizesList2 = sizesList2 + [os.path.getsize(f)]
-            path2 = path2 + [f]
-        else:
-            dirList2 = dirList2 + [f]
+    dirList.sort()
 
-    dirList1.sort()
-    dirList2.sort()
+    return sizesList, path, dirList
 
 # Verifica se as subpastas são de mesmo nome e se sim, as envia para a função principal para serem comparadas também
+def compFolders(dirList1, dirList2):
     for l1 in dirList1:
         for l2 in dirList2:
             if os.path.basename(l1) == os.path.basename(l2):
                 folderManager(l1, l2)
-
-    return sizesList1, sizesList2, path1, path2
