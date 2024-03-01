f3d99c4acc43fd415c657859a2dce4ee54ee09a9
Davi-RD <davirdavanzo@gmail.com>
2024-02-29 21:10:50 -0300

    Melhoras no instalador

--- InstallUpgrade.sh
+++ InstallUpgrade.sh
@@ -1,4 +1,7 @@
-if [ -d "/usr/share/CompBV" ]; then
-    rm -r /usr/share/CompBV && rm /usr/share/applications/CompBV.desktop
+if [ -d "$HOME/.local/share/CompBV" ]; then
+    rm -r $HOME/.local/share/CompBV && rm $HOME/.local/share/applications/CompBV.desktop
 fi
-cp -r CompBV /usr/share && cp CompBV.desktop /usr/share/applications
+cp -r CompBV $HOME/.local/share/ && cp CompBV.desktop $HOME/.local/share/applications
+
+echo Path=$HOME/.local/share/CompBV >> $HOME/.local/share/applications/CompBV.desktop
+
